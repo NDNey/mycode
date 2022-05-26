689928b04dd5096ec5497fe646dc669abe55ae02
NDNey <ndavidney@gmail.com>
2022-05-26 16:29:43 -0400

    challeng post and get

--- flaskchallenge/flaskchallenge.py
+++ flaskchallenge/flaskchallenge.py
@@ -8,25 +8,44 @@ from flask import redirect
 from flask import url_for
 from flask import request
 from flask import render_template
+import requests
+
+
 
 app = Flask(__name__)
+def trivia():
+    url = "https://the-trivia-api.com/api/questions?categories=film_and_tv,music,science&limit=1&region=US"
+    data = requests.get(url)
+    result = data.json()
+    answer = result[0]["correctAnswer"]
+    incorrect_answer = result[0]["incorrectAnswers"]
+   
+    trivia = {
+        "answer" : answer,
+        "choices" : incorrect_answer ,
+        "question" : result[0]["question"]
+    }
+    
+    return trivia
 
 @app.route("/") 
 def main():
-    return render_template("index.html") 
+    
+    try:
+        triviadata = trivia()
+        return render_template("index.html", **triviadata) 
+    except Exception as err:
+        f"Uh-oh! {err}"
+   
 
-@app.route("/correct")
-def correct():
-    return "your answer is correct!"
+
 
 @app.route("/answer", methods = ["POST"])
-def login():
+def answered():
 
     if request.method == "POST":
-        if request.form.get("answer").lower() == 'brazil': 
-            return redirect(url_for("correct"))
-        else: 
-            return redirect(url_for("main"))
+        return redirect(url_for("main"))
+    
    
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=2224) # runs the application
