70fece75c13e4d9b75e7cb87c2b8e72f9509e57d
NDNey <ndavidney@gmail.com>
2022-05-27 17:07:03 -0400

    final project added

--- miniproject/finalProject/alta3research-flask01.py
+++ miniproject/finalProject/alta3research-flask01.py
@@ -3,47 +3,90 @@
 
 ## best practice says don't use commas in imports
 # use a single line for each import
+ 
+from flask import session
 from flask import Flask
 from flask import redirect
 from flask import url_for
 from flask import request
 from flask import render_template
+from flask import jsonify
 
 
 data = [{
-    "entry":"ksdalkjf",
-    "definition":"jksnd"
+    "entry":"Flask",
+    "definition":"Flask is a micro web framework written in Python. It is classified as a microframework because it does not require particular tools or libraries.",
+    "user":"default"
+},{
+    "entry":"Python",
+    "definition":"Python is a high-level, interpreted, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
+    "user":"default"
 }
-
 ]
 
 app = Flask(__name__)
- 
+app.secret_key = "any random string"
 @app.route("/") 
 def main():
-    
     try:
-        print(data)
-        return render_template("index.html") 
+        return render_template("profile.html", entries = data,user = session["username"]) 
     except Exception as err:
         f"Uh-oh! {err}"
 
-@app.route("/newEntry",methods = ["GET","POST"])
-def post():
+@app.route("/entries" )
+def entries():
     try:
+        return render_template("entries.html",entries = data) 
+    except Exception as err:
+        f"Uh-oh! {err}"
+
+
+@app.route("/newEntry",methods = ["GET","POST"])
+def entry():
+    try:
+        
         if request.method == "POST":
             entry = request.form.get("entry") 
             definition = request.form.get("definition")
-            data.append({"entry":entry,"definition":definition})
+            data.append({"entry":entry,"definition":definition,"user" :session["username"]})
             return redirect(url_for("main"))
 
-        else: # if a user sent a post without nm then assign value defaultuser
-          return render_template("form.html" ) 
+        else:
+            if "username" in session:
+                username = session["username"]
+                return render_template("form.html",user = username)
+            else:
+                return render_template("form.html")
+                
     except Exception as err:
         f"Uh-oh! {err}"
 
+@app.route("/search",methods = ["GET","POST"])
+def search():
+    try:
+        if request.method == "POST":
+            search = request.form.get("search").lower() 
+            print(search)
+            search_result = []
+            for item in data:
+                if search in item["entry"].lower():
+                    search_result.append(item)
+            print(search_result)
+            return render_template("entries.html",entries = search_result) 
+    except Exception as err:
+        f"Uh-oh! {err}"
 
     
-   
+@app.route("/user", methods = ["POST"])
+def login():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        session["username"] = request.form.get("name")
+        return redirect(url_for("entry"))
+
+@app.route("/getJson")
+def getJson():
+    return jsonify(data)
+
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=2224) # runs the application
--- miniproject/finalProject/alta3research-requests02.py
+++ miniproject/finalProject/alta3research-requests02.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
 import requests
-import json
 from pprint import pprint
 
-URL= "http://127.0.0.1:2224/"
+URL= "http://127.0.0.1:2224/getJson"
 
-new_hero= {
-           "name": "Wolverine",
-           "realName": "James Howlett",
-           "since": 1974,
-           "powers": ["adamantium skeleton","claws","regeneration"]
-          }
+data = requests.get(URL).json()
 
-# json.dumps takes a python object and returns it as a JSON string
-new_hero= json.dumps(new_hero)
+print("this is an encyclopedia:")
+for item in data:
+  
+    print(f"*Entry Creator : {item['user']} ")
+    print(f"*{item['entry']} : {item['definition']}")
 
-# requests.post requires two arguments at the minimum;
-# a url to send the request 
-# and a json string to attach to the request
-resp= requests.post(URL, json=new_hero)
-
-# pretty-print the response back from our POST request
-pprint(resp.json())
