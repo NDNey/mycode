47a627b3eb66201071add4b2fa2a8571b4c70ad4
NDNey <ndavidney@gmail.com>
2022-05-27 17:18:51 -0400

    final project added

--- miniproject/finalProject/alta3research-flask01.py
+++ miniproject/finalProject/alta3research-flask01.py
@@ -10,79 +10,86 @@ from flask import jsonify
 
 
 data = [{
-    "entry":"Flask",
-    "definition":"Flask is a micro web framework written in Python. It is classified as a microframework because it does not require particular tools or libraries.",
-    "user":"default"
-},{
-    "entry":"Python",
-    "definition":"Python is a high-level, interpreted, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
-    "user":"default"
+    "entry": "Flask",
+    "definition": "Flask is a micro web framework written in Python. It is classified as a microframework because it does not require particular tools or libraries.",
+    "user": "default"
+}, {
+    "entry": "Python",
+    "definition": "Python is a high-level, interpreted, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
+    "user": "default"
 }
 ]
 
 app = Flask(__name__)
 app.secret_key = "any random string"
-@app.route("/") 
+
+
+@app.route("/")
 def main():
     try:
-        return render_template("profile.html", entries = data,user = session["username"]) 
+        return render_template("profile.html", entries=data, user=session["username"])
     except Exception as err:
         f"Uh-oh! {err}"
 
-@app.route("/entries" )
+
+@app.route("/entries")
 def entries():
     try:
-        return render_template("entries.html",entries = data) 
+        return render_template("entries.html", entries=data)
     except Exception as err:
         f"Uh-oh! {err}"
 
 
-@app.route("/newEntry",methods = ["GET","POST"])
+@app.route("/newEntry", methods=["GET", "POST"])
 def entry():
     try:
-        
+
         if request.method == "POST":
-            entry = request.form.get("entry") 
+            entry = request.form.get("entry")
             definition = request.form.get("definition")
-            data.append({"entry":entry,"definition":definition,"user" :session["username"]})
+            data.append({"entry": entry, "definition": definition,
+                        "user": session["username"]})
             return redirect(url_for("main"))
 
         else:
             if "username" in session:
                 username = session["username"]
-                return render_template("form.html",user = username)
+                return render_template("form.html", user=username)
             else:
                 return render_template("form.html")
-                
+
     except Exception as err:
         f"Uh-oh! {err}"
 
-@app.route("/search",methods = ["GET","POST"])
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     try:
         if request.method == "POST":
-            search = request.form.get("search").lower() 
+            search = request.form.get("search").lower()
             print(search)
             search_result = []
             for item in data:
                 if search in item["entry"].lower():
                     search_result.append(item)
             print(search_result)
-            return render_template("entries.html",entries = search_result) 
+            return render_template("entries.html", entries=search_result)
     except Exception as err:
         f"Uh-oh! {err}"
 
-    
-@app.route("/user", methods = ["POST"])
+
+@app.route("/user", methods=["POST"])
 def login():
     if request.method == "POST":
         print(request.form.get("name"))
         session["username"] = request.form.get("name")
         return redirect(url_for("entry"))
 
+
 @app.route("/getJson")
 def getJson():
     return jsonify(data)
 
+
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=2224) # runs the application
+    app.run(host="0.0.0.0", port=2224)  # runs the application
--- miniproject/finalProject/alta3research-requests02.py
+++ miniproject/finalProject/alta3research-requests02.py
@@ -2,13 +2,12 @@
 import requests
 from pprint import pprint
 
-URL= "http://127.0.0.1:2224/getJson"
+URL = "http://127.0.0.1:2224/getJson"
 
 data = requests.get(URL).json()
 
 print("this is an encyclopedia:")
 for item in data:
-  
+
     print(f"*Entry Creator : {item['user']} ")
     print(f"*{item['entry']} : {item['definition']}")
-
